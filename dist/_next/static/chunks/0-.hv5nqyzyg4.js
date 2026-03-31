6c9c36090f47422b25774072ccc787df3701958b
root <root@srv1337402.hstgr.cloud>
2026-03-31 10:49:58 +0000

    Include dist folder for deployment

---
+++ dist/_next/static/chunks/0-.hv5nqyzyg4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/0o3-eb5e8hcv_.js",
+  "static/chunks/1620g17jrzy-n.js",
+  "static/chunks/turbopack-0xw3iwtcb0-kn.js"
+])
---
+++ dist/_next/static/chunks/0r2p9jliaflw3.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/0~~.x_dwhdk5k.js",
+  "static/chunks/1620g17jrzy-n.js",
+  "static/chunks/turbopack-028v0adr.ddas.js"
+])
